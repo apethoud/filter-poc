0f3d8ead2c7b42e12e1c24c78c0323f2a43045ca
Andrew Pethoud <andrewpethoud@gmail.com>
2025-11-13 08:47:41 -0800

    remove all but one page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { WarmColorsComponent } from './components/warm-colors/warm-colors.component';
-import { CoolColorsComponent } from './components/cool-colors/cool-colors.component';
-import { TriadColorsComponent } from './components/triad-colors/triad-colors.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const routes: Routes = [
@@ -15,16 +13,6 @@ export const routes: Routes = [
     path: 'warm-colors',
     component: WarmColorsComponent,
     title: 'Warm Colors',
-  },
-  {
-    path: 'cool-colors',
-    component: CoolColorsComponent,
-    title: 'Cool Colors',
-  },
-  {
-    path: 'triad-colors',
-    component: TriadColorsComponent,
-    title: 'Triad Colors',
   },
   {
     path: '**',
