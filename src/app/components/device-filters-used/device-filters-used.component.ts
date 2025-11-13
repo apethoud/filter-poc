dac9339392fbe369bfe1d7c08bdeadcddc7e0466
Andrew Pethoud <andrewpethoud@gmail.com>
2025-11-13 11:56:42 -0800

    pass device controller to child components

--- src/app/components/device-filters-used/device-filters-used.component.ts
+++ src/app/components/device-filters-used/device-filters-used.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { DeviceFilterController } from '../../controllers/device-filter-controller';
 
 @Component({
   selector: 'app-device-filters-used',
   standalone: true,
   imports: [],
   templateUrl: './device-filters-used.component.html',
-  styleUrl: './device-filters-used.component.css'
+  styleUrl: './device-filters-used.component.css',
 })
 export class DeviceFiltersUsedComponent {
-
+  @Input({ required: true }) filterController?: DeviceFilterController;
 }
--- src/app/components/device-list/device-list.component.ts
+++ src/app/components/device-list/device-list.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { DeviceFilterController } from '../../controllers/device-filter-controller';
 
 @Component({
   selector: 'app-device-list',
   standalone: true,
   imports: [],
   templateUrl: './device-list.component.html',
-  styleUrl: './device-list.component.css'
+  styleUrl: './device-list.component.css',
 })
 export class DeviceListComponent {
-
+  @Input({ required: true }) filterController?: DeviceFilterController;
 }
