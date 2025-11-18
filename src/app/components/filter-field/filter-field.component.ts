2fceb8eccfd768c2c305c7216ce8b93fb7e32c79
Andrew Pethoud <andrewpethoud@gmail.com>
2025-11-18 15:00:23 -0800

    clean up filter field component

--- src/app/components/filter-field/filter-field.component.ts
+++ src/app/components/filter-field/filter-field.component.ts
@@ -1,12 +1,5 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
-// import {
-//   FilterKind,
-//   FilterObservables,
-//   IStringFilterOption,
-// } from '../../models/filter';
-import { Observable, Subscription } from 'rxjs';
+import { Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-// import { FilterOptionType } from '../../models/filter';
 
 @Component({
   selector: 'app-filter-field',
@@ -15,32 +8,11 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './filter-field.component.html',
   styleUrl: './filter-field.component.css',
 })
-export class FilterFieldComponent implements OnInit, OnDestroy {
+export class FilterFieldComponent {
   @Input({ required: true }) name?: string;
   @Input({ required: true }) options?: string[];
   @Input({ required: true }) selectedValue?: string | null;
   @Input({ required: true }) setSelectedValue?: (val: string) => void;
-  // ################## How can I store the selected site value (before form submission) in a way that the zone field can access and respond to it?
-
-  // @Input({ required: true }) saveSelectedValue?: (val: FilterOptionType) => void;
-  // @Input({ required: true }) triggerSave$: Observable<void>;
-
-  // @Input({ required: true }) filterData?: FilterObservables;
-  // @Input({ required: true }) triggerSave?: Observable<void>;
-  // filterName: string = 'Brightness';
-  // filterKind: FilterKind = FilterKind.String;
-  // options?: IStringFilterOption[];
-  // selectedValue?: string;
-  // private readonly _subs: Subscription = new Subscription();
-
-  ngOnInit(): void {
-    //   this.setupFilter();
-    //   this.subscribeToSaveEvents();
-  }
-
-  ngOnDestroy(): void {
-    //   this._subs.unsubscribe();
-  }
 
   handleFilterFieldChange = (event: Event) => {
     if (!this.setSelectedValue) {
@@ -49,59 +21,4 @@ export class FilterFieldComponent implements OnInit, OnDestroy {
     const target = event.target as HTMLSelectElement;
     this.setSelectedValue(target.value);
   };
-
-  // setupFilter = () => {
-  //   if (!this.filterData || this.filterData.kind !== 'string') {
-  //     return;
-  //   }
-
-  //   const optionsObservable$ = this.filterData.optionsObservable;
-  //   const selectedOptionsObservable$ =
-  //     this.filterData.selectedOptionsObservable;
-  //   if (!optionsObservable$ || !selectedOptionsObservable$) {
-  //     return;
-  //   }
-
-  //   const optionsSub = optionsObservable$.subscribe((options) => {
-  //     this.options = options;
-  //   });
-  //   this._subs.add(optionsSub);
-
-  //   const selectedOptionsSub = selectedOptionsObservable$.subscribe(
-  //     (selectedOptions) => {
-  //       if (!selectedOptions || !selectedOptions[0]) {
-  //         return;
-  //       }
-  //       this.selectedValue = selectedOptions[0].toString();
-  //     }
-  //   );
-  //   this._subs.add(selectedOptionsSub);
-  // };
-
-  // subscribeToSaveEvents = () => {
-  //   if (!this.triggerSave) {
-  //     return;
-  //   }
-
-  //   this.triggerSave.subscribe(() => {
-  //     this.saveSelectedValueToController();
-  //   });
-  // };
-
-  // saveSelectedValueToController = () => {
-  //   if (!this.filterData || !this.selectedValue) {
-  //     return;
-  //   }
-  //   const numberArrayPayload = [parseInt(this.selectedValue)];
-  //   console.log('AAP typeof this.selectedValue: ', typeof this.selectedValue);
-  //   this.filterData.selectedOptionsUpdater(numberArrayPayload);
-  // };
-
-  // on form save trigger, call the selectedOptionsUpdater with the new values (if any)
-  // subscribe to the selectedOptions observable to update the dropdown view with the selected value (don't do it on option select)
-  // i can use the same selectedValue variable for both the selected value and the pending selected value because new updates
-  // coming in via the subscription should overwrite the current selected value and the pending selected value won't be overwritten
-  // until a new value comes in via the subscription
-  // --> update the main content view in response to the saved filter value
-  // --> replicate everywhere
 }
