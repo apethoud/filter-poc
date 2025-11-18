a83805867dfec030a9186b209d048819c8e47f47
Andrew Pethoud <andrewpethoud@gmail.com>
2025-11-18 14:56:50 -0800

    clean up device filter menu

--- src/app/components/device-filter-menu/device-filter-menu.component.ts
+++ src/app/components/device-filter-menu/device-filter-menu.component.ts
@@ -1,13 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FilterFieldComponent } from '../filter-field/filter-field.component';
-// import {
-//   FilterListType,
-//   FilterObservables,
-//   FilterTypeEnum,
-// } from '../../models/filter';
-import { Subject } from 'rxjs';
 import { DeviceFilterController } from '../../controllers/device-filter-controller';
-import { FilterOptionType, ISite, IZone } from '../../models/filter';
 import {
   iSiteArrayToSiteNameStringArrayMapper,
   iZoneArrayToZoneNameStringArrayMapper,
@@ -24,17 +17,11 @@ import {
 })
 export class DeviceFilterMenuComponent implements OnInit {
   @Input({ required: true }) filterController?: DeviceFilterController;
-  // @Input({ required: true }) filterList?: FilterListType;
-  // readonly triggerSave$ = new Subject<void>();
   siteOptions: string[] = [];
   zoneOptions: string[] = [];
 
   selectedSiteName?: string;
   selectedZoneName?: string;
-
-  // showBrightnessFilter?: boolean;
-
-  // brightnessFilterData?: FilterObservables;
 
   ngOnInit(): void {
     this.prepareSiteOptions();
@@ -42,19 +29,6 @@ export class DeviceFilterMenuComponent implements OnInit {
 
     this.prepareSelectedSite();
     this.prepareSelectedZone();
-    //   if (!this.filterList) {
-    //     return;
-    //   }
-    //   this.showBrightnessFilter = this.filterList.has(FilterTypeEnum.Brightness);
-    //   if (this.showBrightnessFilter) {
-    //     const brightnessFilterData = this.filterList.get(
-    //       FilterTypeEnum.Brightness
-    //     );
-    //     if (!brightnessFilterData) {
-    //       return;
-    //     }
-    //     this.brightnessFilterData = brightnessFilterData;
-    //   }
   }
 
   // Prepare Field Options
@@ -117,45 +91,10 @@ export class DeviceFilterMenuComponent implements OnInit {
     this.filterController.pendingFilterParams.zone = selectedZone;
   };
 
-  // setPendingSelectedSite = (val: ISite) => {
-  //   if (!this.filterController?.pendingFilterParams?.site) {
-  //     return;
-  //   }
-  //   this.filterController.pendingFilterParams.site = val;
-  //   console.log(
-  //     'this.filterController.pendingFilterParams.site: ',
-  //     this.filterController.pendingFilterParams.site
-  //   );
-  // };
-
-  // setPendingSelectedZone = (val: IZone) => {
-  //   console.log('OOOOO val: ', val);
-  //   // if (!this.filterController?.pendingFilterParams?.zone) {
-  //   //   return;
-  //   // }
-  //   if (!this.filterController) {
-  //     return;
-  //   }
-  //   this.filterController.pendingFilterParams.zone = val;
-  //   console.log(
-  //     'this.filterController.pendingFilterParams.zone: ',
-  //     this.filterController.pendingFilterParams.zone
-  //   );
-  // };
-
-  // setSelectedValue = (val: FilterOptionType) => {
-  //   if (isZone(val)) {
-  //     this.setPendingSelectedZone(val);
-  //   } else {
-  //     this.setPendingSelectedSite(val);
-  //   }
-  // }
-
   triggerSaveAction = (): void => {
     if (!this.filterController) {
       return;
     }
     this.filterController.savePendingFilterParams();
-    // this.triggerSave$.next();
   };
 }
